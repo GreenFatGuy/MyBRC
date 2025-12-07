0f9218c1a1c9efb2b0ae579cd15b905a961581d2
Pavel <samyginpk@gmail.com>
2025-12-08 00:27:53 +0300

    clang format. static link and full lto

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -xeuo pipefail
 
-clang++-20 -fno-exceptions -Wall -Werror -O3 -g -fno-omit-frame-pointer -march=native -flto=thin -std=c++23 main.cpp -o brc
+clang++-20 -static -fno-exceptions -Wall -Werror -O3 -g -fno-omit-frame-pointer -march=native -flto -std=c++23 main.cpp -o brc
 
